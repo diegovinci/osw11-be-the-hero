9c295a37a9ddf1e081ad1c428c89208bde912235
Diego Vinciguerra <diego.vinciguerra@sensedia.com>
2020-03-28 19:38:12 -0300

    feat: add register page

--- frontend/src/pages/Logon/index.js
+++ frontend/src/pages/Logon/index.js
@@ -1,5 +1,7 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import { FiLogIn } from 'react-icons/fi';
+
 import './styles.css';
 
 import logoImg from '../../assets/logo.svg'
@@ -16,11 +18,11 @@ export default function Logon() {
 
           <input placeHolder="Sua ID" />
           <button className="button" type="submit">Entrar</button>
-        
-          <a href="/register">
+
+          <Link className="back-link" to="/register">
             <FiLogIn size={16} color="#E02041" />
             Não tenho cadastro
-          </a>
+          </Link>
         </form>
 
       </section>
