4b6a8cd01321591581912fcac82ab17e6b5b62ba
Diego Vinciguerra <diego.vinciguerra@sensedia.com>
2020-03-28 22:34:26 -0300

    feat: add new incident page

--- frontend/src/pages/Profile/index.js
+++ frontend/src/pages/Profile/index.js
@@ -11,7 +11,7 @@ export default function Profile() {
     <div className="profile-container">
       <header>
         <img src={logoImg} alt="Be the Hero" />
-        <span>Bem vindo, APAD</span>
+        <span>Bem-vinda, APAD</span>
 
         <Link className="button" to="/incidents/new">Cadastrar novo caso</Link>
         <button type="button">
